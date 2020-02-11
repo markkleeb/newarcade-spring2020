f2613368f2e1448925e5907fe2db164d194f657b
markkleeb <kleeb930@gmail.com>
2020-02-11 14:50:00 -0500

    Week 2

---
+++ Week 2/Assets/yoshiScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class yoshiScript : MonoBehaviour
+{
+
+    public KeyCode moveLeft;
+    public KeyCode moveRight;
+    public float speed = 10;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if (Input.GetKeyDown(moveLeft)) {
+        if (Input.GetAxis("Horizontal") < 0 )
+        { 
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+
+        // if (Input.GetKeyDown(moveRight))
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+
+        if (Input.GetAxis("Jump") > 0)
+        {
+            transform.Translate(Vector3.up * speed * Time.deltaTime);
+        }
+
+       
+    }
+}
