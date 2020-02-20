47976a4cd469f98b402f9fd7746b51558cc23d7b
markkleeb <kleeb930@gmail.com>
2020-02-20 16:46:59 -0500

    Week 4 - Animations

---
+++ Week 2/Assets/Scripts/yoshiScript.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class yoshiScript : MonoBehaviour
+{
+
+    public KeyCode moveLeft;
+    public KeyCode moveRight;
+    public float speed = 10;
+
+    private bool facingRight = true;
+    private float horizontal;
+
+    public Animator yoshiAnimator;
+    private SpriteRenderer yoshiSpriteRenderer;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        yoshiSpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        horizontal = Input.GetAxis("Horizontal");
+
+        yoshiAnimator.SetFloat("Speed", Mathf.Abs(horizontal));
+
+        //flip();
+
+        // Yoshi is moving to the left
+        // if (Input.GetKeyDown(moveLeft)) {
+        if (horizontal < 0 )
+        { 
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
+            facingRight = false;
+        }
+
+        //Yoshi is moving to the right
+        // if (Input.GetKeyDown(moveRight))
+        if (horizontal > 0)
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+            facingRight = true;
+        }
+
+        if (Input.GetAxis("Jump") > 0)
+        {
+            transform.Translate(Vector3.up * speed * Time.deltaTime);
+        }
+
+
+        if (facingRight)
+        {
+            yoshiSpriteRenderer.flipX = false;
+        }
+        else
+        {
+            yoshiSpriteRenderer.flipX = true;
+        }
+       
+    }
+
+    void flip()
+    {
+        //if he is moving left AND facing right, OR moving right AND facing left - FLIP
+        if(horizontal > 0 && !facingRight || horizontal < 0 && facingRight)
+        {
+            facingRight = !facingRight;
+            Vector3 position = transform.localScale;
+            position.x *= -1;
+            transform.localScale = position;
+        }
+
+    }
+}
